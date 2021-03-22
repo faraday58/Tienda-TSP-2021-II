07f52d362da051c12acaa5e390def2ce8d45bc46
Armando Rodríguez <jarodriguezvera@gmail.com>
2021-03-22 01:24:14 -0600

    Recuperación de los datos. Cambiando color de los menús, manejando fragments y una listview

--- app/src/main/java/mx/unam/ingenieria/tienda/fragments/InicioFragment.java
+++ app/src/main/java/mx/unam/ingenieria/tienda/fragments/InicioFragment.java
@@ -4,21 +4,44 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+
 import mx.unam.ingenieria.tienda.R;
 
 public class InicioFragment extends Fragment {
 
-
+    ListView lstvProductos;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v= inflater.inflate(R.layout.fragment_inicio,container,false);
+        lstvProductos= v.findViewById(R.id.lstvProductos);
+        llenarLista();
+
         return v;
     }
+
+    private void llenarLista() {
+       String [] productos= {
+               "Tenis",
+               "Zapatos",
+               "Plataformas"
+       };
+
+        ArrayAdapter miAdapter= new ArrayAdapter(getContext(),
+                android.R.layout.simple_list_item_1,
+                productos);
+        lstvProductos.setAdapter(miAdapter);
+
+    }
+
+
 }
