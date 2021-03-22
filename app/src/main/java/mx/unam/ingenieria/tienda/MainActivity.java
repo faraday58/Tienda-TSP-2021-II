6ada1e61c9eabc3f98e6371d7d37fdede4ef24dd
Armando Rodríguez <jarodriguezvera@gmail.com>
2021-03-22 01:01:35 -0600

    Recuperación de los datos. Cambiando color de los menús, manejando fragments y una listview

--- app/src/main/java/mx/unam/ingenieria/tienda/MainActivity.java
+++ app/src/main/java/mx/unam/ingenieria/tienda/MainActivity.java
@@ -1,14 +1,55 @@
 package mx.unam.ingenieria.tienda;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import mx.unam.ingenieria.tienda.fragments.InicioFragment;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView btmNavigationPrincipal;
+    private InicioFragment inicioFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btmNavigationPrincipal=findViewById(R.id.btmNavigationPrincipal);
+        inicioFragment= new InicioFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContenedor,inicioFragment).commit();
+        btmNavigationPrincipal.setOnNavigationItemSelectedListener(navListener);
+
     }
+
+    BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId())
+            {
+                case (R.id.nav_home):
+                    Toast.makeText(getApplicationContext(),"Inicio",Toast.LENGTH_SHORT).show();
+                    break;
+                case (R.id.nav_buscar):
+                    Toast.makeText(getApplicationContext(),"Buscando",Toast.LENGTH_SHORT).show();
+                    break;
+                case (R.id.nav_comprar):
+                    Toast.makeText(getApplicationContext(),"Carrito de compras",Toast.LENGTH_SHORT).show();
+                    break;
+                case (R.id.nav_inicia):
+                    Toast.makeText(getApplicationContext(),"Registro",Toast.LENGTH_SHORT).show();
+                    break;
+
+            }
+
+            return true;
+        }
+    };
+
+
+
 }
---
+++ app/src/main/java/mx/unam/ingenieria/tienda/fragments/InicioFragment.java
@@ -0,0 +1,24 @@
+package mx.unam.ingenieria.tienda.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import mx.unam.ingenieria.tienda.R;
+
+public class InicioFragment extends Fragment {
+
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v= inflater.inflate(R.layout.fragment_inicio,container,false);
+        return v;
+    }
+}
