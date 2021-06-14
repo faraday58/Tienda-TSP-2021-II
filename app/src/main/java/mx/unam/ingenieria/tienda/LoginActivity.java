c3882dc71ff73934a810e20e616d601607991414
Armando Rodríguez <jarodriguezvera@gmail.com>
2021-06-14 02:52:39 -0500

    Guardando Preferencias

--- app/src/main/java/mx/unam/ingenieria/tienda/LoginActivity.java
+++ app/src/main/java/mx/unam/ingenieria/tienda/LoginActivity.java
@@ -1,8 +1,12 @@
 package mx.unam.ingenieria.tienda;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -31,8 +35,57 @@ public class LoginActivity extends AppCompatActivity {
         usuario=this.getIntent().getStringExtra("usuario");
         password=this.getIntent().getStringExtra("password");
 
+        cargarPreferencias();
+
     }
 
+    private void cargarPreferencias() {
+        SharedPreferences preferences= getSharedPreferences("credenciales", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor= preferences.edit();
+
+        String user= preferences.getString("user","Ingrese su nombre de usuario");
+        String pass= preferences.getString("pass","");
+        editTextTextPersonName.setText(user);
+        editTextTextPassword.setText(pass);
+
+    }
+
+
+
+    public AlertDialog dialogoGuardarUsuario(){
+        AlertDialog.Builder builder= new AlertDialog.Builder(LoginActivity.this);
+        builder.setMessage("Indique si desea guardar su \n \n" +
+                "nombre de usuario y contraseña " +
+                "en este dispositivo")
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                    iniciarMain();
+            }
+        })
+                .setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        SharedPreferences preferences= getSharedPreferences("credenciales", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor= preferences.edit();
+                        editor.putString("user",usuario);
+                        editor.putString("pass",password);
+                        editor.commit();
+                        iniciarMain();
+
+                    }
+                });
+
+        return builder.create();
+    }
+
+
+    private void iniciarMain(){
+        Toast.makeText(getApplicationContext(),"Ingreso de manera correcta",Toast.LENGTH_SHORT).show();
+        Intent intentMainActivity = new Intent(LoginActivity.this,MainActivity.class  );
+        startActivity(intentMainActivity);
+        finish();
+    }
 
     View.OnClickListener onClickListenerIngresar = new View.OnClickListener() {
         @Override
@@ -41,10 +94,7 @@ public class LoginActivity extends AppCompatActivity {
 
             if( usuario.equals( editTextTextPersonName.getText().toString())   && password.equals(editTextTextPassword.getText().toString())  )
             {
-                Toast.makeText(getApplicationContext(),"Ingreso de manera correcta",Toast.LENGTH_SHORT).show();
-                Intent intentMainActivity = new Intent(LoginActivity.this,MainActivity.class  );
-                startActivity(intentMainActivity);
-                finish();
+                dialogoGuardarUsuario().show();
             }else
             {
                 Toast.makeText(getApplicationContext(),"Usuario o contraseña incorrectos",Toast.LENGTH_SHORT).show();
