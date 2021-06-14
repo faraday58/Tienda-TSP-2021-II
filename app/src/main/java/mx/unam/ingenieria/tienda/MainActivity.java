1c6504590147ee1f5d18cd2ec4505212a0d1dc26
Armando Rodríguez <jarodriguezvera@gmail.com>
2021-06-13 23:42:29 -0500

    Cambiando el icono de la aplicación para lanzarla desde el celular

--- app/src/main/java/mx/unam/ingenieria/tienda/MainActivity.java
+++ app/src/main/java/mx/unam/ingenieria/tienda/MainActivity.java
@@ -1,14 +1,68 @@
 package mx.unam.ingenieria.tienda;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import mx.unam.ingenieria.tienda.fragments.BuscarFragment;
+import mx.unam.ingenieria.tienda.fragments.ComprarFragment;
+import mx.unam.ingenieria.tienda.fragments.InicioFragment;
+import mx.unam.ingenieria.tienda.fragments.SesionFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private BottomNavigationView btmNavigationPrincipal;
+    private InicioFragment inicioFragment;
+    private BuscarFragment buscarFragment;
+    private SesionFragment sesionFragment;
+    private ComprarFragment comprarFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btmNavigationPrincipal= findViewById(R.id.btmNavigationPrincipal);
+        inicioFragment= new InicioFragment();
+        buscarFragment= new BuscarFragment();
+        sesionFragment= new SesionFragment();
+        comprarFragment= new ComprarFragment();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContenedor,inicioFragment).commit();
+
+        btmNavigationPrincipal.setOnNavigationItemSelectedListener(navListener);
+
     }
+
+    BottomNavigationView.OnNavigationItemSelectedListener navListener= new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            switch (item.getItemId())
+            {
+                case  R.id.nav_home:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContenedor,inicioFragment).commit();
+                    break;
+                case R.id.nav_buscar:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContenedor,buscarFragment).commit();
+                    break;
+                case R.id.nav_comprar:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContenedor,comprarFragment).commit();
+                    break;
+                case R.id.nav_inicia:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContenedor,sesionFragment).commit();
+            }
+
+            return true;
+        }
+    };
+
+
+
+
 }
---
+++ app/src/main/java/mx/unam/ingenieria/tienda/fragments/BuscarFragment.java
@@ -0,0 +1,23 @@
+package mx.unam.ingenieria.tienda.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import mx.unam.ingenieria.tienda.R;
+
+public class BuscarFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_buscar,container,false);
+
+        return v;
+    }
+}
